1111adb5ca4e5707544bba1bc8b6e4e07b160dbe
Robert Brunson <robert.brunson@ext.superleague.com>
2022-11-13 14:26:52 -0500

    fix cache stuff

--- src/util/xivapi.ts
+++ src/util/xivapi.ts
@@ -8,7 +8,7 @@ export const fetchItemData = async () => {
     // download items.json
     const rawDB = await axios.get(ITEMS_DB);
     if (rawDB.data) {
-        await writeFile(ITEM_DB_CACHE_PATH, JSON.stringify(rawDB.data), 'utf-8');
+        await writeFile(ITEM_DB_CACHE_PATH, JSON.stringify(rawDB.data), {encoding: 'utf-8'});
     }
 
     return JSON.parse(await readFile(ITEM_DB_CACHE_PATH, "utf-8"));
